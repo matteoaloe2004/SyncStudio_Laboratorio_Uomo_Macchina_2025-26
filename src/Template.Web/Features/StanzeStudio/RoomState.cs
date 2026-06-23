b6af2267c44d171d32f92001ebe4ae9b6c98a66b
Matteo Aloè <m.aloe@cosmobile.com>
2026-06-23 09:42:17 +0200

    fixing

--- src/Template.Web/Features/StanzeStudio/RoomState.cs
+++ src/Template.Web/Features/StanzeStudio/RoomState.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace Template.Web.Features.StanzeStudio
 {
